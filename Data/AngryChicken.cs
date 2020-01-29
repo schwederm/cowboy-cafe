19b83f2cb8b40f7af17590c65d2308972371607b
Matt Schweder <schwederm@ksu.edu>
2020-01-29 14:41:00 -0600

    Completed AngryChicken and RustlersRibs. Created PecosPulledPork

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class AngryChicken
+    {
+        /// <summary>
+        /// If the sandwich comes with bread
+        /// </summary>
+        public bool Bread { get; set; } = true;
+
+        /// <summary>
+        /// If the sandwich comes with a pickle
+        /// </summary>
+        public bool Pickle { get; set; } = true;
+
+        /// <summary>
+        /// The price of the sandwich
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 6.10;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the sandwich
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 171;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the sandwich
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold sour pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the ribs
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                return instructions;
+            }
+        }
+    }
+}
