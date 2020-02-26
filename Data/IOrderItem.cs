d218b9d10e1323b02fa482fc269699d796ed7a8d
Matt Schweder <schwederm@ksu.edu>
2020-02-26 14:53:12 -0600

    Added OrderTest, IOrderItem, and Order

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// The price for the order item
+        /// </summary>
+        double Price { get; }
+
+        /// <summary>
+        /// The special instruction for the order item
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
